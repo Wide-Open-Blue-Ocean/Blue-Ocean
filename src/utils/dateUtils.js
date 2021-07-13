87efd101d3f3983f9923521ce897f2279ab3edf9
anthony3662 <anthonyye3662@hotmail.com>
2021-07-13 17:36:02 -0400

    added date utils

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -11,6 +11,12 @@ const dateUtils = {
   getWeekday: function(yyyymmdd) {
     var currentDate = new Date(yyyymmdd.slice(0,4), parseInt(yyyymmdd.slice(4,6)) - 1, yyyymmdd.slice(6,8), 12);
     return currentDate.toDateString().split(' ')[0];
+  },
+  today: function() {
+    var currentTime = new Date(Date.now());
+    currentTime.setHours(12); //prevents any errors that could be caused by DST
+    var today = currentTime.getFullYear() + (currentTime.getMonth() + 1).toString().padStart(2, '0') + currentTime.getDate().toString().padStart(2, '0');
+    return today;
   }
 };
 export default dateUtils;
