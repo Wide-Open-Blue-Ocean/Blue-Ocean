10e24d41021216e8a6f1ba0381f6318a33102cca
daveanue <66977330+daveanue@users.noreply.github.com>
2021-07-14 16:24:09 -0400

    latest changes to login view

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/home'
@@ -8,26 +8,58 @@ import Workout from './components/workout'
 import Meals from './components/meals'
 import Journal from './components/journal'
 import Navbar from './components/Navbar/navbar'
+import SignIn from './SignIn.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  return (
-      <>
-        <div >
-          <Navbar />
-        </div>
-        <Switch>
-          <Route path='/' component={Home} exact/>
-          <Route exact path='/calendar' component={window.screen.width <= 600 ? MobileCalendar : Calendar}/>
-          <Route path='/workout' component={Workout}/>
-          <Route path='/meals' >
-            <Meals />
-          </Route>
-          <Route path='/journal' component={Journal}/>
-          <Route exact path='/' component={Home}/>
-        </Switch>
-      </>
-  )
+
+//   const [loggedIn, setLogIn] = useState(false)
+
+// let signedOutView = (
+//   <>
+//     <SignIn setLogIn={setLogIn}/>
+//   </>)
+// let signedInView = (
+//       <>
+//         <div >
+//           <Navbar />
+//         </div>
+//         <Switch>
+//           <Route path='/' component={Home} exact/>
+//           <Route exact path='/calendar' component={window.screen.width <= 600 ? MobileCalendar : Calendar}/>
+//           <Route path='/workout' component={Workout}/>
+//           <Route path='/meals' >
+//             <Meals />
+//           </Route>
+//           <Route path='/journal' component={Journal}/>
+//           <Route exact path='/' component={Home}/>
+//         </Switch>
+//       </>)
+
+// console.log(loggedIn)
+// return loggedIn ? signedInView : signedOutView
+return (
+<>
+<div >
+
+</div>
+<Switch>
+  <Route path='/' component={Home} exact/>
+  <React.Fragment>
+  <Navbar />
+
+  <Route exact path='/calendar' component={window.screen.width <= 600 ? MobileCalendar : Calendar}/>
+  <Route path='/workout' component={Workout}/>
+  <Route path='/meals' >
+    <Meals />
+  </Route>
+  <Route path='/journal' component={Journal}/>
+  <Route exact path='/' component={Home}/>
+</React.Fragment>
+</Switch>
+</>)
+
 }
+
 
 export default App
--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -4,25 +4,28 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import axios from 'axios';
 import ChatEngineComponent from './ChatEngine';
 
-const config = {
+// const config = {
+
+// };
+// firebase.initializeApp(config);
+
+export const auth = firebase.initializeApp({
   apiKey: "AIzaSyDCFYRW6rFX51Y1tvAZNlyfsk6GId7OXuM",
   authDomain: "my-blue-ocean.firebaseapp.com",
   projectId: "my-blue-ocean",
   storageBucket: "my-blue-ocean.appspot.com",
   messagingSenderId: "926790538030",
   appId: "1:926790538030:web:61671ab94de4ef48fe20bf"
-};
-firebase.initializeApp(config);
-
-
+})
 
 const uiConfig = {
   // Popup signin rather than redirect .
   signInFlow: 'popup',
-  callbacks: {
-    // Avoid redirects after sign-in
-    signInSuccessWithAuthResult: () => false,
-  },
+  signInSuccessUrl: '/',
+  // callbacks: {
+  //   // Avoid redirects after sign-in
+  //   signInSuccessWithAuthResult: () => false,
+  // },
   // display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -34,6 +37,7 @@ const SignIn = () => {
   const [userUID, setuserUID] = useState(null);
   const [userEmail, setuserEmail] = useState(null);
   // Listening to Firebase Auth state to set the local state.
+
   useEffect(() => {
 
     const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
@@ -43,7 +47,6 @@ const SignIn = () => {
       if (user) {
         setuserUID(user.uid);
         setuserEmail(user.email);
-
 
         var data = new FormData();
         data.append('username', user.email);
@@ -76,9 +79,12 @@ const SignIn = () => {
   if (!isSignedIn) {
     return (
       <div>
-        <h1>My App</h1>
-        <p>Please sign-in:</p>
+
+        <div id='login-page'>
+        <div id="login-card">
         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+      </div>
       </div>
     )
   }
@@ -86,6 +92,7 @@ const SignIn = () => {
   return (
       <div>
       <h1>My App</h1>
+
       <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
       <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
 
